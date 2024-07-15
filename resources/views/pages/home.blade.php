feb3d17cac9d56748e3639460d1078194e0e5d69
martinaliporata <martinaliporata@gmail.com>
2024-07-15 19:16:29 +0200

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,22 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <img src="{{Vite::asset('resources/img/happy-dogjpg.jpg')}}" alt="">
+    <div class="dog">
+        ciao
+    </div>
+</body>
+</html> --}}
+@extends('layout.app')
+
+@section('main-content')
+    <h1>
+        homepage
+    </h1>
+@endsection
