2ce07e2d60e8fc76790d0aa31d381e60dc5fe0ce
martinaliporata <martinaliporata@gmail.com>
2024-07-16 09:07:24 +0200

    added datas in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,6 +17,28 @@
 
 @section('main-content')
     <h1>
-        homepage
+        @foreach ($books as $book)
+            <article>
+                {{-- <img src="{{Vite::asset('https://www.coverbrowser.com/image/action-comics/1-1.jpg')}}" alt=""> --}}
+                <p>
+                    {{$book['title']}}
+                </p>
+                <p>
+                    {{$book['description']}}
+                </p>
+                <p>
+                    {{$book['price']}}
+                </p>
+                <p>
+                    {{$book['series']}}
+                </p>
+                <p>
+                    {{$book['sale_date']}}
+                </p>
+                <p>
+                    {{$book['type']}}
+                </p>
+            </article>
+        @endforeach
     </h1>
 @endsection
