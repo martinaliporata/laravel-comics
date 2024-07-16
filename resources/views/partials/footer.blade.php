6c8b6ebc55f984e5ef87a39cb77290e66945c499
martinaliporata <martinaliporata@gmail.com>
2024-07-16 08:45:02 +0200

    implementing partials

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,15 @@
 <footer>
-
+    <section class="button">
+        <button>
+            sign-up now!
+        </button>
+    </section>
+    <section class="navbar">
+        <h2>
+            follow us
+        </h2>
+        <div>
+            Navbar social
+        </div>
+    </section>
 </footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <section class="logo">
+        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
+    </section>
+    <section class="navbar">
+    <nav>
+        <ul>
+        <li>
+            Navbar
+        </li>
+        </ul>
+    </nav>
+    </section>
+</header>
